8b798d3c42a35e7c3f7a9bb102c590392d335e46
pedrin0021 <143022030+pedrin0021@users.noreply.github.com>
2023-08-29 21:20:50 -0300

    Calculadora com Radiação e Potenciação

--- calculadora.py
+++ calculadora.py
@@ -6,33 +6,46 @@ while True:
     num1 = int(input("..................Calculadora..................\n\n             Informe o 1º número: "))
 
     #Escolha da operação
-    op = input("\n...................Operações...................\n\n                 (+) Adição\n                 (-) Subtração\n                 (*) Multiplicação\n                 (/) Divisão\n                 \n...............................................\n          Informe a operação desejada: ")
+    op = input("\n...................Operações...................\n\n                 (+) Adição\n                 (-) Subtração\n                 (*) Multiplicação\n                 (/) Divisão\n                 (&) Potenciação\n                 (#) Radiciação\n...............................................\n          Informe a operação desejada: ")
 
-    #Inserção do segundo número
-    num2 = int(input("\n...............................................\n             Informe o 2º número: "))
+    #Estrutura para identificar se o usuário tenha escolhido a opção "(#) Radiciação"
+    if op == '#':
+        result = num1 ** 0.5
+        print('\n..................Resultado..................\n\n                     √' + str(result))
+        sleep(2)
+        os.system('cls') or None
 
-    #Adição
-    if op == '+':
-        result = num1 + num2
-    
-    #Subtração
-    elif op == '-':
-        result = num1 - num2
-    
-    #Multiplicação
-    elif op == '*':
-        result = num1 * num2
+    #Estrutura para as demais operações
+    else:
+        #Inserção do segundo número
+        num2 = int(input("\n...............................................\n             Informe o 2º número: "))
 
-    #Divisão
-    elif op == '/':
-        if num2 == 0:
-            result = 'Impossível!'
-        else:
-            result = num1 / num2
-    
-    #Impressão do resultado
-    print('\n..................Resultado..................\n\n                 ' + str(num1) + ' ' + str(op) + ' ' + str(num2) + ' = ' + str(result))
+        #Adição
+        if op == '+':
+            result = num1 + num2
+        
+        #Subtração
+        elif op == '-':
+            result = num1 - num2
+        
+        #Multiplicação
+        elif op == '*':
+            result = num1 * num2
 
-    #Ferramentas utilizadas para esperar 2 segundos antes de limpar o console 
-    sleep(2)
-    os.system('cls') or None
+        #Divisão
+        elif op == '/':
+            if num2 == 0:
+                result = 'Impossível!'
+            else:
+                result = num1 / num2
+        
+        #Potenciação
+        elif op == '&':
+            result = num1 ** num2
+        
+        #Impressão do resultado
+        print('\n..................Resultado..................\n\n                 ' + str(num1) + ' ' + str(op) + ' ' + str(num2) + ' = ' + str(result))
+
+        #Ferramentas utilizadas para esperar 2 segundos antes de limpar o console 
+        sleep(2)
+        os.system('cls') or None
